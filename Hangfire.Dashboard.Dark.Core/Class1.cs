0ff46651b0a69f917bab7da178e9d18336e158b4
Ahmet Köylü <ahmet.koylu@softtech.com.tr>
2018-12-13 23:04:10 +0300

    Initial commit

--- Hangfire.Dashboard.Dark.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hangfire.Dashboard.Dark.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ Hangfire.Dashboard.Dark.Core/GlobalConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+using Hangfire.Dashboard.Dark.Core.Support;
+
+namespace Hangfire.Dashboard.Dark.Core
+{
+    /// <summary>
+    /// Provides extension methods to setup Hangfire.Dashboard.Dark
+    /// </summary>
+    public static class GlobalConfigurationExtensions
+    {
+        /// <summary>
+        /// Configures Hangfire to use the dark dashboard theme
+        /// </summary>
+        /// <param name="configuration">Global configuration</param>
+        public static IGlobalConfiguration UseDarkDashboard(this IGlobalConfiguration configuration)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            // register dispatchers for CSS
+            var assembly = typeof(GlobalConfigurationExtensions).GetTypeInfo().Assembly;
+            DashboardRoutes.Routes.Append("/css[0-9]+", new EmbeddedResourceDispatcher(assembly, "Hangfire.Dashboard.Dark.Core.Resources.style.css"));
+
+            return configuration;
+        }
+    }
+}
